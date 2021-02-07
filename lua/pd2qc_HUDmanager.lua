22c81363dbddebd9a17dc596a89a9ecaaba7c3a6
markjsosnowski <markjsosnowski@gmail.com>
2021-02-07 02:41:35 -0500

    updated size values

--- lua/pd2qc_HUDmanager.lua
+++ lua/pd2qc_HUDmanager.lua
@@ -40,7 +40,7 @@ function PD2QC:CreatePanelFromTable(table)
     local hint_panel_settings = {}
 
     hint_panel_settings.min_width = GetMinWidth(table)
-    hint_panel_settings.min_height = 100
+    hint_panel_settings.min_height = 97.5
     hint_panel_settings.padding = 5
     
     hint_panel_settings.center_x = PD2QC._center_x
